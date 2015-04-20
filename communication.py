9a351a70a68c0b0b756a7f4ebec3673db01f9846
Maxim Kuznetsov <maks.kuznetsov@gmail.com>
2015-04-20 18:13:54 +0300

    Make segmentation mark subclass of Record. Add test for message classes.
    * communication.py: Make segmentation mark subclass of Record. Add some
    type and value tests.
    * tests/test_messages.py: Add test for message classes.

--- communication.py
+++ communication.py
@@ -39,6 +39,9 @@ class Channel:
         return len(self.queue)
 
 
+#------------------------------------------------------------------------------
+
+
 class Message:
     """
     Superclass of all types of messages
@@ -55,64 +58,6 @@ class Message:
                              'of message.')
 
 
-
-class SegmentationMark(Message):
-
-    def __init__(self, n=None, brackets=None):
-        # Number of opening and closing brakets.
-        if n is not None:
-            self.n = n
-        elif brackets is not None:
-            closing = False
-            n = 0
-            nb = 0
-            for c in brackets:
-                if c == ')' and not closing:
-                    nb += 1
-                elif c == '(' and not closing:
-                    n = nb
-                    nb -= 1
-                    closing = True
-                elif c == '(' and closing:
-                    nb -= 1
-                else:
-                    raise ValueError('Wrong sequence of brackets.')
-
-            if nb != 0:
-                raise ValueError('Wrong sequence of brackets.')
-
-            self.n = n
-
-        self.content = self.__repr__()
-
-    def is_segmark(self):
-        return True
-
-    def plus(self):
-        if self.n > 0:
-            self.n += 1
-        self.content = self.__repr__()
-
-    def minus(self):
-        if self.n > 0:
-            self.n -= 1
-        else:
-            raise ValueError('Negative sequence depth.')
-        self.content = self.__repr__()
-
-    def end_of_stream(self):
-        return True if (self.n == 0) else False
-
-    def __repr__(self):
-        if self.n > 0:
-            return (")" * self.n) + ("(" * self.n)
-        else:
-            return "sigma_0"
-
-    def __str__(self):
-        return self.__repr__()
-
-
 class NilMessage(Message):
 
     def __init__(self):
@@ -120,9 +65,6 @@ class NilMessage(Message):
 
     def __repr__(self):
         return 'nil()'
-
-    def __str__(self):
-        return self.__repr__()
 
 
 class DataMessage(Message):
@@ -137,28 +79,48 @@ class DataMessage(Message):
     def __repr__(self):
         return "msg(" + str(self.content) + ")"
 
-    def __str__(self):
-        return self.__repr__()
-
 
 class Record(DataMessage):
 
-    def __init__(self, content):
-        if type(content) != dict:
-            raise ValueError('Record data must be a dictionary.')
+    def __init__(self, content={}):
+
+        if type(content) is not dict:
+            raise TypeError('Record data must be a dictionary.')
+
         super(Record, self).__init__(content)
 
     def union(self, msg):
         self.content.update(msg.content)
 
-    def extract(self, pattern, tail):
+    def extract(self, pattern, tail=None):
+
+        if not isinstance(pattern, collections.Iterable) or isinstance(pattern, str):
+            raise TypeError('Pattern must be an interable container of labels.')
+
+        for p in pattern:
+            if type(p) is not str:
+                raise TypeError('Labels for must be strings.')
+            if not p:
+                raise ValueError('Labels must be non-empty strings.')
+
+        pattern = set(pattern)
 
         aliases = {}
 
         if pattern:
             aliases.update({l: self.content[l] for l in pattern})
 
-        if tail:
+        if tail is not None:
+
+            if type(tail) is not str:
+                raise TypeError('Tail label must be string.')
+
+            if not tail:
+                raise ValueError('Tail label must be non-empty string.')
+
+            if tail in aliases:
+                raise IndexError('Tail label must not coincide with matched labels.')
+
             label_diff = self.content.keys() - set(pattern)
             tail_content = {l: self.content[l] for l in label_diff}
             aliases.update({tail: tail_content})
@@ -170,6 +132,93 @@ class Record(DataMessage):
 
     def __getitem__(self, index):
         return self.content[index]
+
+    def __setitem__(self, index, value):
+        self.content[index] = value
+
+    def __len__(self):
+        return len(self.content)
+
+    def __repr__(self):
+        return 'record(%s)' % ', '.join('%s: %s' % (k, v)
+                                        for k, v in self.content.items())
+
+
+class SegmentationMark(Record):
+
+    def __init__(self, n=None, nstr=None):
+
+        if n is None and nstr is None:
+            raise ValueError('Depth not provided.')
+
+        super(SegmentationMark, self).__init__()
+        self.n = n if n is not None else self._str_to_n(nstr)
+
+    @property
+    def n(self):
+        return self['__n__']
+
+    @n.setter
+    def n(self, x):
+
+        if type(x) is not int:
+            raise TypeError('Bracketing depth must be an integer.')
+
+        if x < 0:
+            raise ValueError('Bracketing depth must be positive or zero.')
+
+        self['__n__'] = x
+
+    def _str_to_n(self, string):
+
+        if type(string) is not str:
+            raise TypeError('Bracketing representation must be a string.')
+
+        if string == '' or string == 'sigma_0':
+            return 0
+
+        closing = False
+        n = 0
+        nb = 0
+        for c in string:
+            if c == ')' and not closing:
+                nb += 1
+
+            elif c == '(' and not closing:
+                n = nb
+                nb -= 1
+                closing = True
+
+            elif c == '(' and closing:
+                nb -= 1
+
+            else:
+                raise ValueError('Wrong sequence of brackets.')
+
+        if nb != 0:
+            raise ValueError('Wrong sequence of brackets.')
+
+        return n
+
+    def is_segmark(self):
+        return True
+
+    def plus(self):
+        if self.n > 0:
+            self.n += 1
+
+    def minus(self):
+        if self.n > 0:
+            self.n -= 1
+        else:
+            raise ValueError('Negative sequence depth.')
+
+    def end_of_stream(self):
+        return self.n == 0
+
+    def __repr__(self):
+        return (')' * self.n) + ('(' * self.n) if self.n else 'sigma_0'
+
 
 class Empty(Exception):
     def __init__(self, value):
