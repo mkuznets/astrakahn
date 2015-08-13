7960950b7f1c17134f9f630a050b72a00ba9d954
Maxim Kuznetsov <maks.kuznetsov@gmail.com>
2015-08-13 20:44:38 +0100

    tmp: pool.py: add profiling

--- pool.py
+++ pool.py
@@ -6,13 +6,16 @@ import collections
 import os
 import marshal
 import types
+import time
 
 import communication as comm
 
 Result = collections.namedtuple('Result',
-                                'vertex_id action dispatch aux_data')
+                                'vertex_id action dispatch aux_data tm')
 
-def core_wrapper(core, task_data):
+def core_wrapper(core, task_data, timestp):
+
+    tlaunch = time.time() - timestp
 
     code = marshal.loads(core[1])
     core = types.FunctionType(code, globals(), core[0])
@@ -20,10 +23,14 @@ def core_wrapper(core, task_data):
     vertex_id = task_data['vertex_id']
     args = task_data['args']
 
+    #print(os.getpid(), args[0].keys(), core.__name__)
+
+    st = time.time()
     output = core(*args)
+    timecore = time.time() - st
 
     if output is None:
-        return Result(vertex_id, '', {}, None)
+        return Result(vertex_id, '', {}, None, (timestp, timecore, tlaunch, time.time()))
 
     else:
         action, dispatch, aux_data = output
@@ -31,7 +38,7 @@ def core_wrapper(core, task_data):
         dispatch = {p: [comm.Record(msg) for msg in stream]
                     for p, stream in dispatch.items()}
 
-        return Result(vertex_id, action, dispatch, aux_data)
+        return Result(vertex_id, action, dispatch, aux_data, (timestp, timecore, tlaunch, time.time()))
 
 def print_error(err):
     print("Error in pool:", err)
@@ -55,6 +62,14 @@ class PoolManager:
         self.pm.join()
 
     def dispatch_result(self, result):
+        ot = time.time() - result.tm[0]
+        print(result.vertex_id, "\t", '%07f' % ot, "\t"
+
+              "\t", '%06.2f%%' % (result.tm[1]/ot * 100),
+              "\t", '%06.2f%%' % (result.tm[2]/ot * 100),
+              "\t", '%06.2f%%' % ((time.time() - result.tm[3])/ot * 100)
+        )
+
         self.out_queue.put(result)
 
     def enqueue(self, core, task_data):
@@ -68,7 +83,8 @@ class PoolManager:
 
         while True:
             task = self.in_queue.get()
-            pool.apply_async(core_wrapper, task, callback=self.dispatch_result,
+            ttask = task + (time.time(), )
+            pool.apply_async(core_wrapper, ttask, callback=self.dispatch_result,
                              error_callback=print_error)
 
         pool.close()
